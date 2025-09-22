c7c7583be40008d82a41af9f48b5fd5e030d423f
Diony Rinker <dionyrinker40@gmail.com>
2025-09-21 21:30:56 -0300

    New ui changes

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -7,30 +7,29 @@ export default function home() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      
       setRandomPosition({
-        x: Math.random() * 100, 
-        y: Math.random() * 100, 
+        x: Math.random() * 100,
+        y: Math.random() * 100,
       });
     }, 1000); // Atualiza a posição a cada 2 segundos
 
-    return () => clearInterval(interval); 
+    return () => clearInterval(interval);
   }, []);
 
   return (
-    <>
-      <div className="p-8">
+    <div className="">
+      <div className="p-8 h-[30vh] bg-[linear-gradient(39deg,rgba(44,0,97,1)_0%,rgba(79,0,176,1)_100%)]">
         <motion.div
           className="w-full flex justify-between"
-          initial={{ opacity: 0, x: 50, y: -20 }} 
-          animate={{ opacity: 1, x: 0, y: 0 }} 
+          initial={{ opacity: 0, x: 50, y: -20 }}
+          animate={{ opacity: 1, x: 0, y: 0 }}
           transition={{
             duration: 1.4,
-            ease: "easeOut", 
+            ease: "easeOut",
           }}
         >
           <div>
-            <p className="font-[rubikMedium] text-[24px]">Wallet</p>
+            <p className="font-[rubikMedium] text-white text-[24px]">Wallet</p>
             <p className="text-[#BDBDBD]">Active</p>
           </div>
 
@@ -38,28 +37,25 @@ export default function home() {
             <p>usericon</p>
           </div>
         </motion.div>
+      <div className="mt-10 flex-wrap items-center justify-center">
+        <div className="flex justify-center items-center mt-4">
+          <div className="relative">
+          <p className="text-white text-lg absolute top-0 left-[-4vw]">$</p>
+            <p className="text-5xl text-white">298,87</p>
+          </div>
+      </div>
 
-        <div className="mt-10 rounded-[50px] bg-[#5B259F] w-full h-[140px] overflow-hidden relative">
-          <motion.svg
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            animate={{
-              x: `calc(${randomPosition.x}% - 27px)`, 
-              y: `calc(${randomPosition.y}% - 27px)`, 
-            }}
-            transition={{
-              type: "spring",
-              stiffness: 50, 
-              damping: 25,
-            }}
-          >
-            <circle cx="27" cy="27" r="27" fill="#6E34B8" />
-          </motion.svg>
         </div>
       </div>
-    </>
+          {/* termino header */}
+
+
+          {/* Começo body */}
+      <div className="h-[70vh] bg-white w-screen relative flex justify-center">
+          <div className="w-[90vw] h-[10vh] bg-white absolute top-[-4vh] rounded-tr-2xl rounded-tl-2xl">
+
+          </div>
+      </div>
+    </div>
   );
 }
